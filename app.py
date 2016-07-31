108b955ee5bf3dbc25ef6c3c5eb24431082e064f
Mikhael <litchevm@gmail.com>
2016-07-30 21:13:52 -0700

    Switched up files

--- app.py
+++ app.py
@@ -7,21 +7,21 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-	return render_template('index.html') #all templates in folder
+	return 'Hello world' #render_template('index.html') #all templates in folder
 
 @app.route("/first")
 def second():
 	return "Hello World"
 
 #returns website
-@app.route("/<username>")
-def profile(username):
-	return render_template('profile.html', name = username)
+#@app.route("/<username>")
+#def profile(username):
+#	return render_template('profile.html', name = username)
 
 
-@app.route("/second/<username>") #creating username as a variable
-def third(username): #username is ARGV
-	return "Hello " + username + "!!!" #magic!
+#@app.route("/second/<username>") #creating username as a variable
+#def third(username): #username is ARGV
+#	return "Hello " + username + "!!!" #magic!
 
 
 if __name__ == "__main__":
--- run.py
+++ run.py
@@ -1,5 +1,8 @@
+"""
 import os from pml 
 import app 
 
 port = int(os.environ.get('PORT', 5000)) 
 app.run(host='0.0.0.0', port=port)
+
+"""
