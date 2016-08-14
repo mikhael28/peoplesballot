0e1610c93e587ee0ce9e75829a850c99e50b6f9f
IanB <ian.berry6@gmail.com>
2016-08-14 12:50:03 -0700

    Added Bootstrap library into Flask

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 #from flask.ext.sqlalchemy import SQLAlchemy
+def create_app():
+	app = Flask(__name__)
+	Bootstrap(app)
 
-app = Flask(__name__)
+	return app
+
+
 #app.config.from_pyfile('config.py')
 #db = SQLAlchemy(app)
 
